bc0b65850fe715b8447b443c9622f551c1434ffe
Syafiq Unijaya <syafiqazwan.unijaya@gmail.com>
2026-05-11 11:51:33 +0800

    WIP

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use SyafiqUnijaya\AiChatbox\Http\Controllers\AdminController;
 
 Route::get('/', [AdminController::class, 'index'])->name('ai-chatbox.admin.index');
-Route::get('/conversations', [AdminController::class, 'conversations'])->name('ai-chatbox.admin.conversations');
-Route::get('/conversations/data', [AdminController::class, 'conversationsData'])->name('ai-chatbox.admin.conversations.data');
-Route::get('/conversations/{id}/messages', [AdminController::class, 'conversationMessages'])->name('ai-chatbox.admin.conversations.messages');
+Route::get('conversations', [AdminController::class, 'conversations'])->name('ai-chatbox.admin.conversations');
+Route::get('conversations/data', [AdminController::class, 'conversationsData'])->name('ai-chatbox.admin.conversations.data');
+Route::get('conversations/{id}/messages', [AdminController::class, 'conversationMessages'])->name('ai-chatbox.admin.conversations.messages');
--- routes/rag.php
+++ routes/rag.php
@@ -5,5 +5,6 @@ use SyafiqUnijaya\AiChatbox\Http\Controllers\RagController;
 
 Route::get('/', [RagController::class, 'index'])->name('ai-chatbox.rag.index');
 Route::post('/', [RagController::class, 'store'])->name('ai-chatbox.rag.store');
+
 Route::delete('/{id}', [RagController::class, 'destroy'])->name('ai-chatbox.rag.destroy');
 Route::post('/{id}/reprocess', [RagController::class, 'reprocess'])->name('ai-chatbox.rag.reprocess');
